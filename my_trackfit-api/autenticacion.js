ac3bf0729df8ebad803ee5df87abc9e600af7351
santivalverde4 <167050226+santivalverde4@users.noreply.github.com>
2025-05-08 12:47:07 -0600

    Agregación sign up

--- my_trackfit-api/autenticacion.js
+++ my_trackfit-api/autenticacion.js
@@ -4,14 +4,10 @@ import { useMock } from "../app";
 import ErrorHandler from "../utils/ErrorHandler";
 
 class AuthService {
-    //Inicia sesión de un usuario usando sus credenciales.
+  // Inicia sesión de un usuario usando sus credenciales.
   async loginUser(credentials) {
-    const {
-      Username: username,
-      Password: password,
-    } = credentials;
+    const { Username: username, Password: password } = credentials;
 
-     // Define los parámetros de entrada para el procedimiento almacenado
     const params = {
       inUsername: [username, TYPES.VarChar],
       inPassword: [password, TYPES.VarChar],
@@ -21,7 +17,6 @@ class AuthService {
       if (useMock) {
         return { success: true, Id: 1, Username: "test" };
       } else {
-        // Ejecuta el procedimiento almacenado 'sp_login'
         const response = await execute("sp_login", params, {});
         if (response.output.outResultCode == 0) {
           let data = response.recordset[0];
@@ -40,7 +35,8 @@ class AuthService {
       throw new Error(`An error occurred while logging in: ${error}`);
     }
   }
-  //Cierra la sesión de un usuario dado su ID.
+
+  // Cierra la sesión de un usuario dado su ID.
   async logoutUser(userId) {
     if (!userId) {
       return false;
@@ -54,7 +50,6 @@ class AuthService {
       if (useMock) {
         return true;
       } else {
-        // Ejecuta el procedimiento almacenado 'sp_logout'
         const response = await execute("sp_logout", params, {});
         if (response.output.outResultCode == 0) {
           return true;
@@ -65,6 +60,32 @@ class AuthService {
     } catch (error) {
       console.error("Error details:", error);
       throw new Error(`An error occurred while logging out: ${error}`);
+    }
+  }
+
+  // Registra un nuevo usuario.
+  async signUpUser(credentials) {
+    const { Username: username, Password: password } = credentials;
+
+    const params = {
+      inUsername: [username, TYPES.VarChar],
+      inPassword: [password, TYPES.VarChar],
+    };
+
+    try {
+      if (useMock) {
+        return { success: true, message: "User created successfully." };
+      } else {
+        const response = await execute("sp_crear_usuario", params, { outResultCode: TYPES.Int });
+        if (response.output.outResultCode === 0) {
+          return { success: true, message: "User created successfully." };
+        } else {
+          return ErrorHandler(response);
+        }
+      }
+    } catch (error) {
+      console.error("Error details:", error);
+      throw new Error(`An error occurred while signing up: ${error}`);
     }
   }
 }
