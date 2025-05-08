a2871346b80174b69100c11106a732e104b4a1c7
santivalverde4 <167050226+santivalverde4@users.noreply.github.com>
2025-05-07 23:13:40 -0600

    Update autenticacion.js

--- my_trackfit-api/autenticacion.js
+++ my_trackfit-api/autenticacion.js
@@ -4,12 +4,14 @@ import { useMock } from "../app";
 import ErrorHandler from "../utils/ErrorHandler";
 
 class AuthService {
+    //Inicia sesión de un usuario usando sus credenciales.
   async loginUser(credentials) {
     const {
       Username: username,
       Password: password,
     } = credentials;
 
+     // Define los parámetros de entrada para el procedimiento almacenado
     const params = {
       inUsername: [username, TYPES.VarChar],
       inPassword: [password, TYPES.VarChar],
@@ -19,6 +21,7 @@ class AuthService {
       if (useMock) {
         return { success: true, Id: 1, Username: "test" };
       } else {
+        // Ejecuta el procedimiento almacenado 'sp_login'
         const response = await execute("sp_login", params, {});
         if (response.output.outResultCode == 0) {
           let data = response.recordset[0];
@@ -37,7 +40,7 @@ class AuthService {
       throw new Error(`An error occurred while logging in: ${error}`);
     }
   }
-
+  //Cierra la sesión de un usuario dado su ID.
   async logoutUser(userId) {
     if (!userId) {
       return false;
@@ -51,6 +54,7 @@ class AuthService {
       if (useMock) {
         return true;
       } else {
+        // Ejecuta el procedimiento almacenado 'sp_logout'
         const response = await execute("sp_logout", params, {});
         if (response.output.outResultCode == 0) {
           return true;
