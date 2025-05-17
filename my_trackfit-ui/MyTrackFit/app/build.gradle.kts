41f06e2d16def743b4af425681a0fe3d78d69f91
Dilan Hernandez <dilanhernandez48@gmail.com>
2025-05-16 21:08:21 -0600

    Buttons functionality added

--- my_trackfit-ui/MyTrackFit/app/build.gradle.kts
+++ my_trackfit-ui/MyTrackFit/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
